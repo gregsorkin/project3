f4a05a0eb3b15ef448d0393366fc272c8757f701
Gino DiVittorio <gino.gjd@icloud.com>
2019-06-30 22:31:12 -0500

    update to data file

--- data/musicians.js
+++ data/musicians.js
@@ -1,47 +1,83 @@
 const musicians = [
     {
-        name: "Marilyn Manson",
-        image: "http://www.providermodule.com/news/uploads/hud-perou3-th.jpg",
+        name: 'Marilyn Manson', //Singer
+        image: 'http://www.providermodule.com/news/uploads/hud-perou3-th.jpg',
         scores: []
     }, {
-        name: "Gene Simmons",
-        image: "https://www.gannett-cdn.com/-mm-/ef5b6ae4a5f1dda929869a48e2e9e4269eeb6758/c=551-0-5761-3917/local/-/media/2018/07/09/USATODAY/USATODAY/636667309176728434-EPA-SPAIN-MUSIC-101317555.JPG?width=540&height=405&fit=crop",
+        name: 'Gene Simmons', //Kiss
+        image: 'https://www.gannett-cdn.com/-mm-/ef5b6ae4a5f1dda929869a48e2e9e4269eeb6758/c=551-0-5761-3917/local/-/media/2018/07/09/USATODAY/USATODAY/636667309176728434-EPA-SPAIN-MUSIC-101317555.JPG?width=540&height=405&fit=crop',
         scores: []
     }, {
-        name: "Nancy Wilson",
-        image: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Nancy_Wilson_Sydney_2011.jpg",
+        name: 'Davey Havok', //AFI
+        image: 'https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/05/GettyImages-679451854_afi_davey_havok_1000.jpg',
         scores: []
     }, {
-        name: "Janis Joplin",
-        image: "https://mypoeticside.com/wp-content/uploads/gallery-images/aafc615.jpeg",
+        name: 'Maria Brink', //In This Moment
+        image: 'https://i.pinimg.com/originals/38/be/57/38be578c38b0a9f79fb20cb665b0e799.jpg',
         scores: []
     }, {
-        name: "Prince",
-        image: "https://s26162.pcdn.co/wp-content/uploads/2016/04/Prince-thumbnail-4.jpg",
+        name: 'Robert Smith', //The Cure
+        image: 'https://d2h1pu99sxkfvn.cloudfront.net/b0/5957743/331909458_hUfvDMuyRj/P6.jpg',
         scores: []
     }, {
-        name: "Robert Smith",
-        image: "https://d2h1pu99sxkfvn.cloudfront.net/b0/5957743/331909458_hUfvDMuyRj/P6.jpg",
+        name: 'Jack White', //White Stripes
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/11/Jack_White_Ottawa.jpg',
         scores: []
     }, {
-        name: "Jack White",
-        image: "https://upload.wikimedia.org/wikipedia/commons/1/11/Jack_White_Ottawa.jpg",
+        name: 'Trent Reznor', //9 Inch Nails
+        image: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1015367702%2F960x0.jpg%3Ffit%3Dscale',
         scores: []
     }, {
-        name: "David Bowie",
-        image: "http://america.aljazeera.com/content/ajam/articles/2016/1/11/david-bowie-a-shapeshifter-whose-genius-survived-every-transformation/jcr:content/image.img.jpg",
+        name: 'Billie Joe Armostrong', //Green Day
+        images: 'https://www.billboard.com/files/styles/article_main_image/public/media/billie-joe-armstrong-2018-billboard-1548.jpg',
         scores: []
     }, {
-        name: "Michael Fitzgerald",
-        image: "https://live.staticflickr.com/6082/6133590373_0fbab5a8bc_b.jpg",
+        name: 'Gerard Way', //My Chemical Romance
+        images: 'https://www.google.com/search?q=gerard+way&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiJnY-0q5LjAhXXX80KHf57CxoQ_AUIECgB&biw=1194&bih=766#imgrc=cIwNNUUBqquTEM:',
         scores: []
     }, {
-        name: "John Gourley",
-        image: "https://upload.wikimedia.org/wikipedia/commons/f/f6/John_Baldwin_Gourley_at_2010_Sasquatch%21_Festival.jpg",
+        name: 'James Hetfield', //Metallica
+        image: 'http://www2.pictures.gi.zimbio.com/James+Hetfield+Live+Earth+London+Show+EEw43jOLPqfl.jpg',
         scores: []
     }, {
-        name: "Brian Mazzaferri",
-        image: "http://i.imgur.com/RLb36Nf.jpg",
+        nam: 'Axl Rose', //Guns N Roses
+        images: 'https://townsquare.media/site/295/files/2018/07/rose.jpg',
+        scores: []
+    }, {
+        name: 'Pat Benatar', //Singer
+        images: 'https://www.biography.com/.image/t_share/MTIwNjA4NjMzNTgwMTI3NzU2/pat-benatar-213028-1-402.jpg',
+        scores: []
+    }, {
+        name: 'Jon Bon Jovi', //Bon Jovi
+        image: 'http://www2.pictures.zimbio.com/gi/Jon+Bon+Jovi+Bon+Jovi+Performs+MGM+Grand+rBUCUiWq-WDl.jpg',
+        scores: []
+    }, {
+        name: 'Tom Delonge', //Blink 182
+        image: 'https://nyppagesix.files.wordpress.com/2017/08/gettyimages-143960825.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+        scores: []
+    }, {
+        name: 'Dan Reynolds', //Imagine Dragons
+        image: 'https://ichef.bbci.co.uk/images/ic/720x405/p0545hfn.jpg',
+        scores: []
+    }, {
+        name: 'Jared Leto', //Thirty Seconds to Mars
+        image: 'https://pixel.nymag.com/imgs/fashion/daily/2014/01/13/13-jared-leto-governors-awards.w700.h700.jpg',
+        scores: []
+    }, {
+        name: 'Rivers Cuomo', //Weezer
+        image: 'https://argonautnews.com/wp-content/uploads/2014/01/3rivers.jpg',
+        scores: []
+    }, {
+        name: 'Ozzy Osbourne', //Black Sabbath
+        image: 'https://komonews.com/resources/media/ee13c342-cc91-46c8-958b-34a1d692afef-large16x9_AP19038474092185.jpg?1549545435063',
+        scores: []
+    }, {
+        name: 'Alex Varkatzas', //Atreyu
+        image: 'https://live.staticflickr.com/5603/15397764849_e7700f9145_b.jpg',
+        scores: []
+    }, {
+        name: 'Jonathan Davis', //Korn
+        image: 'https://i1.wp.com/metalinjection.net/wp-content/uploads/2015/11/Jonathan-Davis-Korn.jpg?fit=700%2C394&ssl=1',
         scores: []
     }
 ]
